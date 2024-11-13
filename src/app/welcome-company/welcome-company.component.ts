78e6ddb96ec7c26826232fdfb1cbdf10780aaa06
Samuel Apolion Benevenuto <sasibenevenuto@gmail.com>
2024-11-13 09:51:13 -0300

    ajuste menu

--- src/app/welcome-company/welcome-company.component.ts
+++ src/app/welcome-company/welcome-company.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import {ToolbarComponent} from '../shared/toolbar/toolbar.component'
 
 @Component({
   selector: 'app-welcome-company',
   standalone: true,
-  imports: [MatGridListModule,MatCardModule,RouterLink,MatButtonModule,MatMenuModule],
+  imports: [MatGridListModule, MatCardModule, RouterLink, MatButtonModule, MatMenuModule,ToolbarComponent],
   templateUrl: './welcome-company.component.html',
   styleUrl: './welcome-company.component.css'
 })
